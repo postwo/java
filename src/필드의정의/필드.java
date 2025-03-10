5779bde35bfeb9969d0b36dabcac2c4bac0c6dfb
postwo <codo7717@naver.com>
2025-03-10 15:27:58 +0900

    필드

---
+++ src/필드의정의/필드.java
@@ -0,0 +1,17 @@
+package 필드의정의;
+
+public class 필드 {
+
+    // 정적 변수
+    public static int classvar =1;
+
+    // 인스턴스 변수
+    private int instanceVar;
+
+    // paramvar 매개변수
+    public static void main(String[] paramaVar){
+
+        //지역변수
+        int localval =10;
+    }
+}
