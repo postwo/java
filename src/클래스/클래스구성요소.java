782ecf7c06ca6f842f459ebbaccf00198ce77d70
postwo <codo7717@naver.com>
2025-03-10 15:23:07 +0900

    클래스 구성요소

---
+++ src/클래스/클래스구성요소.java
@@ -0,0 +1,13 @@
+package 클래스;
+
+public class 클래스구성요소 { //클래스를 이용해 객체(속성=필드 행위=메서드)를 생성
+
+    private String name; //필드
+    private String department; //필드
+
+    //생성자
+    public 클래스구성요소(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+}
